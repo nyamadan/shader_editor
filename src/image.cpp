8993ece34d96256fc510d6a507ef9e246f75c2d4
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-02-15 23:42:55 +0900

    image.cpp

---
+++ src/image.cpp
@@ -0,0 +1,76 @@
+#include <stb_image.h>
+#include "image.hpp"
+#include "app_log.hpp"
+
+Image::Image() {}
+
+Image::~Image() {
+    if (data != nullptr) {
+        stbi_image_free(data);
+        data = nullptr;
+    }
+
+    if (textureId != 0) {
+        glDeleteTextures(1, &textureId);
+        textureId = 0;
+    }
+}
+
+const std::string &Image::getPath() const { return path; }
+
+void Image::setPath(const std::string &base, const std::string &name) {
+    std::string ext = getExtention(base);
+    this->setPath(base, name, ext);
+}
+
+void Image::setPath(const std::string &base, const std::string &name,
+                    const std::string &ext) {
+    this->base = base;
+    this->name = name;
+    this->ext = ext;
+
+    this->path = this->base;
+    appendPath(this->path, this->name);
+}
+
+bool Image::load() {
+    stbi_set_flip_vertically_on_load(1);
+    uint8_t *data = stbi_load(path.c_str(), &width, &height, &channels, 0);
+    if (data == nullptr) {
+        return false;
+    }
+
+    this->data = data;
+
+    if (textureId == 0) {
+        glGenTextures(1, &this->textureId);
+    }
+
+    glBindTexture(GL_TEXTURE_2D, textureId);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+
+    switch (channels) {
+        case 3:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+                         GL_UNSIGNED_BYTE, data);
+            break;
+        case 4:
+            glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA,
+                         GL_UNSIGNED_BYTE, data);
+            break;
+        default:
+            AppLog::getInstance().addLog("Texture Channel Error: %d\n",
+                                         channels);
+            break;
+    }
+
+    return true;
+}
+
+bool Image::isLoaded() const { return this->data != nullptr; }
+
+uint32_t Image::getTexture() const { return this->textureId; }
--- src/image.hpp
+++ src/image.hpp
@@ -1,8 +1,6 @@
 #pragma once
 
-#include <vector>
 #include <string>
-#include <stb_image.h>
 #include "file_utils.hpp"
 
 class Image {
@@ -19,74 +17,15 @@ class Image {
     uint8_t *data = nullptr;
 
    public:
-    Image() {}
-    ~Image() {
-        if (data != nullptr) {
-            stbi_image_free(data);
-            data = nullptr;
-        }
+    Image();
+    ~Image();
+    const std::string &getPath() const;
 
-        if (textureId != 0) {
-            glDeleteTextures(1, &textureId);
-            textureId = 0;
-        }
-    }
-
-    const std::string &getPath() const { return path; }
-
-    void setPath(const std::string &base, const std::string &name) {
-        std::string ext = getExtention(base);
-        this->setPath(base, name, ext);
-    }
-
+    void setPath(const std::string &base, const std::string &name);
     void setPath(const std::string &base, const std::string &name,
-                 const std::string &ext) {
-        this->base = base;
-        this->name = name;
-        this->ext = ext;
+                 const std::string &ext);
+    bool load();
+    bool isLoaded() const;
 
-        this->path = this->base;
-        appendPath(this->path, this->name);
-    }
-
-    bool load() {
-        stbi_set_flip_vertically_on_load(1);
-        uint8_t *data = stbi_load(path.c_str(), &width, &height, &channels, 0);
-        if (data == nullptr) {
-            return false;
-        }
-
-        this->data = data;
-
-        if (textureId == 0) {
-            glGenTextures(1, &this->textureId);
-        }
-
-        glBindTexture(GL_TEXTURE_2D, textureId);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-
-        switch (channels) {
-            case 3:
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
-                             GL_UNSIGNED_BYTE, data);
-                break;
-            case 4:
-                glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
-                             GL_RGBA, GL_UNSIGNED_BYTE, data);
-                break;
-            default:
-                AppLog::getInstance().addLog("Texture Channel Error: %d\n", channels);
-                break;
-        }
-
-        return true;
-    }
-
-    bool isLoaded() const { return this->data != nullptr; }
-
-    uint32_t getTexture() const { return this->textureId; }
+    uint32_t getTexture() const;
 };
