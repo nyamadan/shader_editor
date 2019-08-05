d286c778ec7b358d9bbf1c4138d8dbec0b784acb
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-08-05 17:12:47 +0900

    fix msvc build

--- build_libvpx_vc142.cmd
+++ build_libvpx_vc142.cmd
@@ -1,4 +1,8 @@
-cd libvpx
+PUSHD libvpx
 bash ./configure --target=x86_64-win64-vs15 --disable-vp9 --disable-vp8-decoder --disable-docs --disable-tools --disable-examples
-make || msbuild vpx.vcxproj -m -t:Build -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142
-cd ..
+make
+IF NOT %ERRORLEVEL% == 0 (
+  msbuild vpx.vcxproj -m -t:Build -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142
+)
+POPD
+exit 0
