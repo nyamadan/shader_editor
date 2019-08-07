0ff660a10b63b470f266b7ead062dc674843bdbf
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-08-07 09:23:43 +0900

    fix build for windows

--- build_libvpx_vc142.cmd
+++ build_libvpx_vc142.cmd
@@ -1,7 +1,8 @@
 PUSHD libvpx
-bash -e ./configure "--target=x86_64-win64-vs15 --disable-vp9 --disable-vp8-decoder --disable-docs --disable-tools --disable-examples && make"
+wsl ./configure --target=x86_64-win64-vs15 --disable-vp9 --disable-vp8-decoder --disable-docs --disable-tools --disable-examples
+wsl make
 IF NOT %ERRORLEVEL% == 0 (
   msbuild vpx.vcxproj -m -t:Build -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142
 )
 POPD
-rem exit %ERRORLEVEL%
+exit %ERRORLEVEL%
