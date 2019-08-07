1b8759ae969a8b196fa00880b2e290607749e6bc
Yoshiki YAMADA <nyamadandan@gmail.com>
2019-08-08 08:17:58 +0900

    add reset

--- build_libvpx_vc142.cmd
+++ build_libvpx_vc142.cmd
@@ -1,8 +1,9 @@
 PUSHD libvpx
+wsl git reset --hard HEAD 
 wsl ./configure --target=x86_64-win64-vs15 --disable-vp9 --disable-vp8-decoder --disable-docs --disable-tools --disable-examples
 wsl make
 IF NOT %ERRORLEVEL% == 0 (
   msbuild vpx.vcxproj -m -t:Build -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142
 )
 POPD
-exit %ERRORLEVEL%
+EXIT %ERRORLEVEL%
