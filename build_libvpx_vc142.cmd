f959bf4d9aff928cec4f6bc733aede6442c91cc8
Yoshiki YAMADA <nyamadandan@gmail.com>
2020-03-07 22:23:45 +0900

    Update libvpx

--- build_libvpx_vc142.cmd
+++
@@ -1,9 +0,0 @@
-PUSHD libvpx
-wsl git reset --hard HEAD 
-wsl ./configure --target=x86_64-win64-vs15 --disable-vp9 --disable-vp8-decoder --disable-docs --disable-tools --disable-examples
-wsl make
-IF NOT %ERRORLEVEL% == 0 (
-  msbuild vpx.vcxproj -m -t:Build -p:Configuration=Release -p:Platform=x64 -p:PlatformToolset=v142
-)
-POPD
-COPY /Y libvpx\x64\Release\vpxmd.lib vpxmd.lib
--- src/main.cpp
+++ src/main.cpp
@@ -1378,7 +1378,7 @@ void EnableOpenGLDebugExtention() {
     if (flags & GL_CONTEXT_FLAG_DEBUG_BIT) {
         glEnable(GL_DEBUG_OUTPUT);
         glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
-        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageCallback((GLDEBUGPROC)glDebugOutput, nullptr);
         glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0,
                               nullptr, GL_TRUE);
     }
