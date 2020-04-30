a2055372671794a2d1febb2704e8fd6e9f868c4a
Yoshiki YAMADA <nyamadandan@gmail.com>
2020-04-30 16:33:37 +0900

    update settings

--- settings.js
+++ settings.js
@@ -1,18 +1,16 @@
 Module.arguments = ["-linfo", "./assets"];
 
-document.addEventListener("DOMContentLoaded", (event) => {
-  const copy = document.createElement("textarea");
-  copy.setAttribute("id", "copy");
-  copy.setAttribute("display", "none");
-  document.body.appendChild(copy);
+const copy = document.createElement("textarea");
+copy.setAttribute("id", "copy");
+copy.setAttribute("display", "none");
+document.body.appendChild(copy);
 
-  document.body.addEventListener("paste", (event) => {
-    if (event.target !== document.body) {
-      return;
-    }
+document.body.addEventListener("paste", (event) => {
+  if (event.target !== document.body) {
+    return;
+  }
 
-    const text = (event.clipboardData || window.clipboardData).getData("text");
-    Module.SetClipboardText(text);
-    event.preventDefault();
-  }, false);
+  const text = (event.clipboardData || window.clipboardData).getData("text");
+  Module.SetClipboardText(text);
+  event.preventDefault();
 }, false);
