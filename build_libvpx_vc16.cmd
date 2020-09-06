477aa7165a6f6d721466a4d2fe4d326cf8c9b5df
Yoshiki YAMADA <nyamadandan@gmail.com>
2020-09-06 16:27:02 +0900

    update

--- build_libvpx_vc16.cmd
+++ build_libvpx_vc16.cmd
@@ -1,7 +1,8 @@
 CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 PUSHD libvpx
-SET PATH=C:\msys64\usr\bin;C:\msys64\mingw64\bin;%PATH%
+SET PATH=%MSYS2_PATH%\usr\bin;%MSYS2_PATH%\mingw64\bin;%PATH%
+where bash
 bash ./configure --target=x86_64-win64-vs16 --disable-vp9 --disable-vp8-decoder --disable-docs --disable-tools --disable-examples --disable-unit-tests --disable-decode-perf-tests --disable-encode-perf-tests
 make
 POPD
